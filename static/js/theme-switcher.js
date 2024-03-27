8dd2783c4f42f26f3f6f1acfb969d54008cd6280
rreimets <reigo.reimets@gmail.com>
2024-03-27 22:44:03 +0200

    repaired js file

--- static/js/theme-switcher.js
+++ static/js/theme-switcher.js
@@ -1,4 +1,4 @@
-// leht laetakse sujuvalt sisse
+// leht laetakse sujuvalt sisse ja laeb lehe eelistuse 
 document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('loaded');
 
@@ -6,25 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.setAttribute("data-theme", savedTheme);
 });
 
-
-// Need kaks järgmist koodi lõiku hoolitsevad lehekülje dark/light tekkimise ja püsimise eest
-
+// see koodi lõik hoolitseb lehekülje dark/light tekkimise ja püsimise eest
 document.getElementById("themeToggle").addEventListener("click", function() {
     let currentTheme = document.body.getAttribute("data-theme") === "dark" ? "light" : "dark";
     document.body.setAttribute("data-theme", currentTheme);
     localStorage.setItem('theme', currentTheme); // Salvesta teema eelistus
 });
-
-// Lae teema eelistus, kui leht laaditakse
-document.addEventListener('DOMContentLoaded', () => {
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    document.body.setAttribute("data-theme", savedTheme);
-});
-
-// document.getElementById("themeToggle").addEventListener("click", function() {
-//     if (document.body.getAttribute("data-theme") === "dark") {
-//         document.body.setAttribute("data-theme", "light");
-//     } else {
-//         document.body.setAttribute("data-theme", "dark");
-//     }
-// });
